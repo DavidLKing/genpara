cfff6d89354451e7d9fddd8adb5c299c0d8884ab
David L. King <king.david.lee@gmail.com>
2018-10-21 20:29:12 +0200

    shortcut for elmo

--- alignment scripts/alignSub.py
+++ alignment scripts/alignSub.py
@@ -104,12 +104,48 @@ def writeout(name, lines):
                 of.write('\t'.join(line) + '\n')
 
 ### ELLMO ###
-options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
-weight_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
+# to do: download these
+options_file = "../data/elmo_2x4096_512_2048cnn_2xhighway_options.json"
+weight_file = "../data/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
+# options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
+# weight_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
 
-elmo = Elmo(options_file, weight_file, 2, dropout=0)
+elmo = Elmo(options_file, weight_file, 3, dropout=0)
 # messy prototype
 single_sent_elmo = lambda sent: elmo(batch_to_ids([sent]))
+'''
+(Pdb) len(single_sent_elmo("This is a test"))
+2
+(Pdb) single_sent_elmo("This is a test").keys()
+dict_keys(['elmo_representations', 'mask'])
+(Pdb) single_sent_elmo("This is a test")['mask']
+tensor([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
+(Pdb) single_sent_elmo("This is a test")['elmo_representations'].shape
+*** AttributeError: 'list' object has no attribute 'shape'
+(Pdb) len(single_sent_elmo("This is a test")['elmo_representations'])
+2
+(Pdb) single_sent_elmo("This is a test")['elmo_representations'][0]
+tensor([[[-4.1971e-01, -4.4025e-01, -1.0587e+00,  ...,  7.2652e-02,
+          -7.4392e-01, -4.4630e-01],
+         [-3.3414e-01, -7.8741e-01, -4.8827e-01,  ...,  7.9663e-02,
+           3.8118e-01,  1.9112e-02],
+         [-1.1728e-01, -2.9004e-01, -5.1561e-01,  ..., -1.3266e-01,
+          -9.3138e-02, -2.9839e-01],
+         ...,
+         [ 2.0640e-01,  3.3418e-01, -4.8526e-02,  ...,  4.1011e-02,
+           7.4620e-01,  9.6554e-02],
+         [ 3.9084e-01,  6.2279e-01, -1.3639e-01,  ...,  4.2772e-01,
+           6.9154e-01, -2.4551e-01],
+         [ 8.0042e-02, -9.9265e-02, -4.2147e-01,  ..., -3.7287e-01,
+           1.8058e-01,  2.3906e-01]]], grad_fn=<DropoutBackward>)
+(Pdb) single_sent_elmo("This is a test")['elmo_representations'][0].shape
+torch.Size([1, 14, 1024])
+(Pdb) single_sent_elmo("This is a test")['elmo_representations'][1].shape
+torch.Size([1, 14, 1024])
+(Pdb) single_sent_elmo("This is a test")['elmo_representations'][2].shape
+*** IndexError: list index out of range
+(Pdb) 
+'''
 ### Ready
 pdb.set_trace()
 
