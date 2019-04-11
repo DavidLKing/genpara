ecc452a800995d2a9de420b1b4c7e92d039d9866
David L. King <king.david.lee@gmail.com>
2019-04-10 20:46:34 -0400

    updating score.py

--- alignment scripts/score.py
+++ alignment scripts/score.py
@@ -12,6 +12,25 @@ from allennlp.modules.elmo import Elmo, batch_to_ids
 
 from amad_batcher import MiniBatch
 
+class lang_mod:
+
+    def __init__(self):
+        pass
+
+    def load_lm(self, lmfile):
+        print("Loading language model", lmfile)
+        # print(lmfile[-5:])
+        if lmfile[-4:] == 'arpa':
+            models = arpa.loadf(lmfile)
+            self.lm = models[0]
+            self.score = lambda x: self.lm.log_p(x)
+        elif lmfile[-3:] == 'bin':
+            self.lm = kenlm.LanguageModel(lmfile)
+            self.score = lambda x: self.lm.score(x)
+        else:
+            sys.exit("I can only read kenlm .bin or .arpa file")
+
+
 class Score:
 
     def __init__(self):
@@ -26,62 +45,32 @@ class Score:
         for word in sent:
             if word in model:
                 vecs.append(model[word])
+        mean = np.mean(np.asarray(vecs), axis=0)
+        return mean
+
+    def phrasal_mean_maker(self, sent, model):
+        # also works for single words
+        vecs = []
+        for word in sent:
+            if word in range(len(model)):
+                vecs.append(model[word])
+        if vecs == []:
+            print('vecs = []')
+            pdb.set_trace()
         # print(sent)
         # pdb.set_trace()
         mean = np.mean(np.asarray(vecs), axis=0)
+        if type(mean) != np.ndarray:
+            if np.isnan(mean):
+                print("We got a nan!")
+                print("\tvecs", vecs)
+                print("\tsent", sent)
+                pdb.set_trace()
+        # should we check every value for nans?
         return mean
 
     def david_metric(self, cos, dist):
         return (1 - ((cos + 1) / 2)) * dist
-
-    def elmo_diffs(self, elmo, data):
-        newdata = []
-        print("generating elmo vectors")
-        # srcs = elmo(batch_to_ids([x[2] for x in data]))['elmo_representations'][2]
-        # paras = elmo(batch_to_ids([x[3] for x in data]))['elmo_representations'][2]
-        # for line, src_vec, para_vec in zip(data, srcs, paras):
-        for line in data:
-            src_word = line[0]
-            para_word = line[1]
-            src = line[2]
-            # tgt = line[4]
-            para = line[3]
-            # try:
-            src_idx = src.split().index(src_word)
-            para_idx = para.split().index(para_word)
-            # except:
-            #     pdb.set_trace()
-            dist, cos, joint = self.elmo_word_diff(elmo, src, para, src_idx, para_idx)#, src_vec, para_vec)
-            line = [str(dist), str(cos), str(joint)] + line
-            # print(dist, cos, joint)
-            print('\t'.join(line))
-            newdata.append(line)
-            # pdb.set_trace()
-        return newdata
-
-    def elmo_diffs_batch(self, elmo, data):
-        newdata = []
-        print("generating elmo vectors")
-        srcs = elmo(batch_to_ids([x[2] for x in data]))['elmo_representations'][2]
-        paras = elmo(batch_to_ids([x[3] for x in data]))['elmo_representations'][2]
-        for line, src_vec, para_vec in zip(data, srcs, paras):
-            src_word = line[0]
-            para_word = line[1]
-            src = line[2]
-            # tgt = line[4]
-            para = line[3]
-            # try:
-            src_idx = src.split().index(src_word)
-            para_idx = para.split().index(para_word)
-            # except:
-            #     pdb.set_trace()
-            dist, cos, joint = self.elmo_word_diff(elmo, src, para, src_idx, para_idx, src_vec, para_vec)
-            line = [str(dist), str(cos), str(joint)] + line
-            print('\t'.join(line))
-            # print(dist, cos, joint)
-            newdata.append(line)
-            # pdb.set_trace()
-        return newdata
 
     def rebuild_dialogs(self, corrected):
         dialog = 0
@@ -97,55 +86,120 @@ class Score:
             num_src[sent].append([dialog - 1, turn - 1])
         return num_src
 
-
-    def elmo_word_diff(self, model, src, para, src_idx, para_idx):#, src_vec, para_vec):
-        # src_vecs = model(batch_to_ids([src]))
-        # elmo(batch_to_ids([src]))['elmo_representations'][2]
-        src_word_vec = np.asarray([2][0][src_idx].detach())
-        # src_word_vec = np.asarray(model(batch_to_ids([src]))['elmo_representations'][2][0][src_idx].detach())
-        # para_vecs = model(batch_to_ids([para]))
-        para_word_vec = np.asarray(model(batch_to_ids([para]))['elmo_representations'][2][0][para_idx].detach())
-        # para_word_vec = np.asarray(model(batch_to_ids([para]))['elmo_representations'][2][0][para_idx].detach())
-        # pdb.set_trace()
-        dist = np.linalg.norm(src_word_vec - para_word_vec)
-        cos = self.sim(src_word_vec, para_word_vec)
-        joint = self.david_metric(cos, dist)
-        # pdb.set_trace()
-        return dist, cos, joint
-
     def score_list(self, src_vec, para_vec):
         cos_sim = self.sim(src_vec, para_vec)
         dist = np.linalg.norm(src_vec - para_vec)
         joint = self.david_metric(cos_sim, dist)
         return cos_sim, dist, joint
 
-    def score(self, line, word2vec, glove, elmo_src, elmo_aligned, elmo_orig, elmo_para):
+    def return_idxs(self, sent, word_list):
+        idxs = []
+        # for just getting pattern words
+        for word in word_list:
+                try:
+                    idxs.append(sent.index(word))
+                except:
+                    print('word', word)
+                    print('sent', sent)
+                    pdb.set_trace()
+        return idxs
+
+    def return_other_idxs(self, sent, word_list):
+        idxs = []
+        # for just getting variable words
+        for word in sent:
+            if word not in word_list:
+                try:
+                    idxs.append(sent.index(word))
+                except:
+                    print('word', word)
+                    print('sent', sent)
+                    pdb.set_trace()
+        return idxs
+
+    def list_of_words(self, swapper):
+        try:
+            swapper = [x for x in eval(swapper) if "$" not in x]
+        except:
+            swapper = [swapper]
+        return swapper
+
+    def score(self, line, word2vec, glove, ng_model, elmo_src, elmo_aligned, elmo_orig, elmo_para):
         # defunct
         line = line.lower().strip().split('\t')
-        swappable = line[0]
+        swappable = self.list_of_words(line[0])
+        # print('swappable line', line[0])
+        # should acount for sarah's and my stuff
         # swapped = line[1]
-        para_word = line[1]
+        para_word = self.list_of_words(line[1])
+        # print('para_word line', line[1])
         src = line[2].split(' ')
         aligned = line[3].split(' ')
-        orig = line[5].split(' ')
+        # again, for sarah's output
+        try:
+            orig = eval(line[5])
+        except:
+            orig = line[5].split(' ')
         para = line[4].split(' ')
         # src_word = line[0]
-        src_idx = src.index(swappable)
-        align_idx = aligned.index(para_word)
-        orig_idx = orig.index(swappable)
-        para_idx = para.index(para_word)
+        # print('swappable', swappable)
+        # print('para_word', para_word)
+        # print('src', src)
+        # print('orig', orig)
+        # print('aligned', aligned)
+        # print('para', para)
+        # print("doing src")
+        # return indexes from word in pattern
+        # src_idxes = self.return_idxs(src, swappable)
+        # averaging... why not?
+        src_idxes = [x for x in range(len(src))]
+
+        # print('src_idxes', src_idxes)
+        # pdb.set_trace()
+        # src_idx = src.index(swappable)
+        # print("doing align")
+        # return indexes from word in pattern
+        # align_idxes = self.return_idxs(aligned, para_word)
+        # averaging... why not?
+        align_idxes = [x for x in range(len(aligned))]
+
+        # print('align_idxes', align_idxes)
+        # pdb.set_trace()
+        # align_idx = aligned.index(para_word)
+        # print("doing orig")
+        # return indexes from word not in pattern
+        # orig_idxes = self.return_other_idxs(orig, swappable)
+        # averaging... why not?
+        orig_idxes = [x for x in range(len(orig))]
+
+        # print('orig_idxes', orig_idxes)
+        # pdb.set_trace()
+        # orig_idx = orig.index(swappable)
+        # print("doing para")
+        # return indexes from word not in pattern
+        # para_idxes = self.return_other_idxs(para, para_word)
+        # averaging... why not?
+        para_idxes = [x for x in range(len(para))]
+
+        # print('para_idxes', para_idxes)
+        # pdb.set_trace()
+        # para_idx = para.index(para_word)
         # try:
         # elmo_src_vec = np.asarray(elmo_src[src_idx].detach())
         # elmo_align_vec = np.asarray(elmo_aligned[align_idx].detach())
         # elmo_orig_vec = np.asarray(elmo_orig[orig_idx].detach())
         # elmo_para_vec = np.asarray(elmo_para[para_idx].detach())
-        # only pull non variable words
-        elmo_src_vec = elmo_src[src_idx]
-        elmo_align_vec = elmo_aligned[align_idx]
-        elmo_orig_vec = elmo_orig[orig_idx]
-        elmo_para_vec = elmo_para[para_idx]
+        elmo_src_vec = self.phrasal_mean_maker(src_idxes, elmo_src)
+        # pdb.set_trace()
+        # elmo_src_vec = elmo_src[src_idx]
+        elmo_align_vec = self.phrasal_mean_maker(align_idxes, elmo_aligned)
+        # elmo_align_vec = elmo_aligned[align_idx]
+        elmo_orig_vec = self.phrasal_mean_maker(orig_idxes, elmo_orig)
+        # elmo_orig_vec = elmo_orig[orig_idx]
+        elmo_para_vec = self.phrasal_mean_maker(para_idxes, elmo_para)
+        # elmo_para_vec = elmo_para[para_idx]
         # except:
-        #     pdb.set_trace()
+        # pdb.set_trace()
         # ELMO SRC -> PARA
         elmo_src_para_sim, elmo_src_para_dist, elmo_src_para_david = self.score_list(elmo_src_vec, elmo_para_vec)
         # ELMO SRC -> ORIG
@@ -165,6 +219,14 @@ class Score:
         w2v_orig = self.mean_maker(orig, word2vec)
         w2v_para = self.mean_maker(para, word2vec)
         # TODO is this enought checks or should I check EVERY vector?
+        ng_src = ng_model.score(' '.join(src))
+        ng_orig = ng_model.score(' '.join(aligned))
+        ng_align = ng_model.score(' '.join(orig))
+        ng_para = ng_model.score(' '.join(para))
+        ng_src_para = ng_src - ng_para
+        ng_src_orig = ng_src - ng_orig
+        ng_orig_para = ng_orig - ng_para
+        ng_align_para = ng_align - ng_para
         if not np.isnan(glove_para[0]) and not np.isnan(glove_src[0]) and \
                 not np.isnan(w2v_para[0]) and not np.isnan(w2v_src[0]):
             # pdb.set_trace()
@@ -236,11 +298,16 @@ class Score:
                 elmo_orig_para_david,
                 elmo_align_para_sim, 
                 elmo_align_para_dist, 
-                elmo_align_para_david)
+                elmo_align_para_david,
+                ng_src_para,
+                ng_src_orig,
+                ng_orig_para,
+                ng_align_para)
 
 
 
 s = Score()
+
 
 print("""
     hacky arguments:
@@ -248,7 +315,11 @@ print("""
     2 = glove text file in w2v format
     3 = tsv to score
     4 = original corrected.tsv (2016 VP data)
+    5 = Gigaword 5-gram model
         """)
+
+
+# temp test
 
 corrected = open(sys.argv[4], 'r').readlines()
 dialog_turn_nums = s.rebuild_dialogs(corrected)
@@ -268,10 +339,13 @@ glove = gensim.models.KeyedVectors.load_word2vec_format(sys.argv[2], binary=Fals
 # glove = ''
 
 
+
 ### ELMO ###
 print("loading ELMo...")
-options_file = "../data/elmo_2x4096_512_2048cnn_2xhighway_options.json"
-weight_file = "../data/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
+# options_file = "../data/elmo_2x4096_512_2048cnn_2xhighway_options.json"
+# weight_file = "../data/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
+options_file = "../data/bigelmo/elmo_2x4096_512_2048cnn_2xhighway_5.5B_options.json"
+weight_file = "../data/bigelmo/elmo_2x4096_512_2048cnn_2xhighway_5.5B_weights.hdf5"
 elmo = Elmo(options_file, weight_file, 3, dropout=0)
 m = MiniBatch(options_file,
               weight_file,
@@ -285,6 +359,11 @@ m = MiniBatch(options_file,
 # test_sents = [x.strip().split(' ') for x in open('elmo_singular_swap.src', 'r').readlines()]
 # test_extract = m.extract(test_sents, 3, 32)
 
+# KENLM
+print('loading kenlm gigaword 5gram model')
+lm = lang_mod()
+lm.load_lm(sys.argv[5])
+
 swap_txt = open(sys.argv[3], 'r').readlines()
 print("extracting ELMo representations...")
 swap_csv = pandas.read_csv(sys.argv[3] ,delimiter='\t')
@@ -292,6 +371,9 @@ swap_csv = pandas.read_csv(sys.argv[3] ,delimiter='\t')
 srcs = [x.split() for x in swap_csv['src'].tolist()]
 aligns = [x.split() for x in swap_csv['align'].tolist()]
 origs = [x.split() for x in swap_csv['orig'].tolist()]
+# for sarah's output
+if type(origs[0]) == list:
+    origs = [' '.join(x).replace("['", "").replace("']", "").replace("', '", " ") for x in origs]
 paras = [x.split() for x in swap_csv['para'].tolist()]
 # Non-minibatched
 # elmo_src = elmo(batch_to_ids(srcs))['elmo_representations'][2]
@@ -301,7 +383,7 @@ paras = [x.split() for x in swap_csv['para'].tolist()]
 # mini-batched
 # TODO make 3rd arg, batch size, an option
 
-batch_size = 128
+batch_size = 4
 
 print("Extracting ELMo rep for srcs")
 elmo_src = m.extract(srcs, 2, batch_size)
@@ -318,6 +400,7 @@ outfile = open('scored.tsv', 'w')
 
 header = swap_txt[0].strip().split('\t')
 
+# header = [
 header = ['dialog',
             'turn',
             'glove_src_para_sim',
@@ -355,7 +438,11 @@ header = ['dialog',
             'elmo_orig_para_david',
             'elmo_align_para_sim', 
             'elmo_align_para_dist', 
-            'elmo_align_para_david'] + header
+            'elmo_align_para_david',
+            'ng_src_para',
+            'ng_src_orig',
+            'ng_orig_para',
+            'ng_align_para'] + header
 # print('\t'.join(header))
 outfile.write('\t'.join(header) + '\n')
 
@@ -369,28 +456,45 @@ lost = []
 # TODO add sanity check to make sure lengths are all correct
 # TODO figure out how to get Pandas to output something I can iterate through
 for line in swap_txt[1:]:
+    print("number", total)
     total += 1
     line = line.lower()
     # w2v_sim, glove_sim, elmo_sim, w2v_dist, glove_dist, elmo_dist, w2v_david, glove_david, elmo_david = s.score(line, w2v, glove, elmo_src[str(line_nmr)], elmo_para[str(line_nmr)])
-    sims = s.score(line, w2v, glove,
-               elmo_src[line_nmr],
-               elmo_align[line_nmr],
-               elmo_orig[line_nmr],
-               elmo_para[line_nmr])
+    # score(self, line, word2vec, glove, ng_model, elmo_src, elmo_aligned, elmo_orig, elmo_para)
+    # pdb.set_trace()
+    sims = s.score(line, w2v, glove, lm,
+                   elmo_src[line_nmr],
+                   elmo_align[line_nmr],
+                   elmo_orig[line_nmr],
+                   elmo_para[line_nmr])
     # print('sims', sims)
     # print('line', line)
     # print('\t'.join(list([str(x) for x in sims]) + line.strip().split('\t')))
-    original = line.split('\t')[5]
-    if original in dialog_turn_nums:
+    split_line = line.strip().split('\t')
+    original = split_line[5]
+    print("original", original)
+    if len(split_line) == 13:
+        dia_turn = split_line[-1]
+        dia_turn = eval(dia_turn)
+        dial_num = dia_turn[0]
+        turn_num = dia_turn[1]
+        outfile.write('\t'.join([str(dial_num), str(turn_num)] + list([str(x) for x in sims]) + line.strip().split('\t')) + '\n')
+    elif original in dialog_turn_nums:
         for nums in dialog_turn_nums[original]:
             dial_num = nums[0]
             turn_num = nums[1]
-            outline = '\t'.join([str(dial_num), str(turn_num)] + list([str(x) for x in sims]) + line.strip().split('\t')) + '\n'
-            if 'nan nan' in outline:
-                pdb.set_trace()
-            else:
-                outfile.write(outline)
+            outfile.write('\t'.join([str(dial_num), str(turn_num)] + list([str(x) for x in sims]) + line.strip().split('\t')) + '\n')
+    # Sarah's originals are strings that were once arrays
+    # TODO I don't think these have to be converted, but 
+    # double check
+    elif ' '.join(eval(original)) in dialog_turn_nums:
+        original = ' '.join(eval(original))
+        for nums in dialog_turn_nums[original]:
+            dial_num = nums[0]
+            turn_num = nums[1]
+            outfile.write('\t'.join([str(dial_num), str(turn_num)] + list([str(x) for x in sims]) + line.strip().split('\t')) + '\n')
     else:
+        pdb.set_trace()
         lost.append('\t'.join(list([str(x) for x in sims]) + line.strip().split('\t')) + '\n')
         missing += 1
     line_nmr += 1
@@ -398,56 +502,3 @@ for line in swap_txt[1:]:
 print("missing", missing, "of", total)
 
 
-
-
-
-
-
-### OLD STUFF ###
-# to do: download these
-# options_file = "../data/elmo_2x4096_512_2048cnn_2xhighway_options.json"
-# weight_file = "../data/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
-# options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
-# weight_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
-
-# elmo = Elmo(options_file, weight_file, 3, dropout=0)
-# messy prototype
-# sent = "This is a test"
-# single_sent_elmo = lambda sent: elmo(batch_to_ids([sent]))
-'''
-(Pdb) len(single_sent_elmo("This is a test"))
-2
-(Pdb) single_sent_elmo("This is a test").keys()
-dict_keys(['elmo_representations', 'mask'])
-(Pdb) single_sent_elmo("This is a test")['mask']
-tensor([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
-(Pdb) single_sent_elmo("This is a test")['elmo_representations'].shape
-*** AttributeError: 'list' object has no attribute 'shape'
-(Pdb) len(single_sent_elmo("This is a test")['elmo_representations'])
-2
-(Pdb) single_sent_elmo("This is a test")['elmo_representations'][0]
-tensor([[[-4.1971e-01, -4.4025e-01, -1.0587e+00,  ...,  7.2652e-02,
-          -7.4392e-01, -4.4630e-01],
-         [-3.3414e-01, -7.8741e-01, -4.8827e-01,  ...,  7.9663e-02,
-           3.8118e-01,  1.9112e-02],
-         [-1.1728e-01, -2.9004e-01, -5.1561e-01,  ..., -1.3266e-01,
-          -9.3138e-02, -2.9839e-01],
-         ...,
-         [ 2.0640e-01,  3.3418e-01, -4.8526e-02,  ...,  4.1011e-02,
-           7.4620e-01,  9.6554e-02],
-         [ 3.9084e-01,  6.2279e-01, -1.3639e-01,  ...,  4.2772e-01,
-           6.9154e-01, -2.4551e-01],
-         [ 8.0042e-02, -9.9265e-02, -4.2147e-01,  ..., -3.7287e-01,
-           1.8058e-01,  2.3906e-01]]], grad_fn=<DropoutBackward>)
-(Pdb) single_sent_elmo("This is a test")['elmo_representations'][0].shape
-torch.Size([1, 14, 1024])
-(Pdb) single_sent_elmo("This is a test")['elmo_representations'][1].shape
-torch.Size([1, 14, 1024])
-(Pdb) single_sent_elmo("This is a test")['elmo_representations'][2].shape
-*** IndexError: list index out of range
-(Pdb) 
-VE SRC -> ORIG
-            # GLOVE ORIG -> PARA
-                        # GLOVE ALIGN -> PARA
-                        
-'''
