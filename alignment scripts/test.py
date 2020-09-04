4535319907c3a221cf98854a93986c7f6dc7ee62
David L. King <king.david.lee@gmail.com>
2020-09-03 21:43:40 -0400

    moving old stuff

--- alignment scripts/test.py
+++
@@ -1,9 +0,0 @@
-import pdb
-
-from amad_batcher import MiniBatch
-
-sents = open('gold_singular_swap.src', 'r').readlines()[:256]
-print("Loading elmo")
-m = MiniBatch('../data/elmo_2x4096_512_2048cnn_2xhighway_options.json', '../data/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5', 3)
-test = m.extract(sents, 2, 128)
-pdb.set_trace()
